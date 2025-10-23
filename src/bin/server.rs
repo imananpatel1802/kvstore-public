ddb3b47e3c97bf833b807d1507ba357d8782aa4b
imananpatel1802 <47051365+imananpatel1802@users.noreply.github.com>
2025-10-23 12:59:10 -0500

    Update server.rs
    decreased capacity of response to 4096

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -87,7 +87,7 @@ fn handle_client(
     let mut writer = stream.try_clone().unwrap();
     let reader = BufReader::with_capacity(65536, &stream);
     let mut lines = reader.lines();
-    let mut response = String::with_capacity(10000);
+    let mut response = String::with_capacity(4096);
     let mut log = String::new();
     let mut snapshot_count = 0;
 
